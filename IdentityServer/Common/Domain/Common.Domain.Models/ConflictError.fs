bba8ab9480814294f9f173646a1cd1ed1a8a6fed
baio <max.putilov@gmail.com>
2020-08-14 00:09:28 +0300

    validate domain pool name unique

---
+++ IdentityServer/Common/Domain/Common.Domain.Models/ConflictError.fs
@@ -0,0 +1,13 @@
+﻿namespace Common.Domain.Models
+
+[<AutoOpen>]
+module ForbiddenError =
+    
+    type ForbiddenErrorCode =
+        | UNIQUE
+                
+    type ForbiddenError =
+        | ForbiddenErrorField of string * ForbiddenErrorCode
+        | ForbiddenErrorCommon of string
+
+
