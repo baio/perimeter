33e591a7cececba945810fb3bae5967e9f62811c
baio <max.putilov@gmail.com>
2020-08-26 00:08:34 +0300

    admin routing with getDomainIdFrom

--- WebClients/apps/admin/src/app/app-routing.module.ts
+++ WebClients/apps/admin/src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router'; // CLI imports router
+import { NavigationStart, Router, RouterModule, Routes } from '@angular/router'; // CLI imports router
+import { AuthService } from '@perimeter/ngx-auth';
+import { isNil } from 'lodash/fp';
+import { filter } from 'rxjs/operators';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginCbPageComponent } from './pages/login-cb-page/login-cb-page.component';
 import { AdminPagesModule } from './pages/pages.module';
@@ -16,9 +19,20 @@ const routes: Routes = [
     {
         path: '',
         redirectTo: 'home',
-        pathMatch: 'full'
+        pathMatch: 'full',
     },
 ]; // sets up routes constant where you define your routes
+
+const getDomainIdFromUrl = (url: string) => {
+    if (/^\/tenant\//.test(url)) {
+        return -1;
+    } else if (/^\/domains\/(\d+)/.test(url)) {
+        const m = url.match(/^\/domains\/(\d+)/);
+        return +m[1];
+    } else {
+        return null;
+    }
+};
 
 // configures NgModule imports and exports
 @NgModule({
@@ -31,4 +45,18 @@ const routes: Routes = [
     ],
     exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+    constructor(router: Router, authService: AuthService) {
+        // check if domain was changed by url on navigation start
+        // 1. $\/tenant\/ - 0 domain
+        // 2. $\/domains\/:id - id domain
+        // If they was changed relogin (login user under sso)
+        router.events
+            .pipe(filter((f) => f instanceof NavigationStart))
+            .subscribe((x: NavigationStart) => {
+                const url = x.url;
+                const domainId = getDomainIdFromUrl(url);
+                console.log('+++', domainId);
+            });
+    }
+}
