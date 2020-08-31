c24abac2c0d8e278eded7742af4a6c4898b46775
baio <max.putilov@gmail.com>
2020-08-31 23:40:03 +0300

    authstringsprovider

---
+++ IdentityServer/Apps/PRR/PRR.API/Infra/AuthStringsProvider.fs
@@ -0,0 +1,26 @@
+﻿namespace PRR.API.Infra
+
+open System
+open Common.Domain.Models
+
+[<AutoOpen>]
+module RandomStringProvider =
+    let private random = Random()
+
+    let private getRandomString length =
+        let chars =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+        [ 0 .. length ]
+        |> Seq.map (fun x -> chars.[random.Next(chars.Length)])
+        |> System.String.Concat
+
+
+    let private authStringsProvider: AuthStringsProvider =
+        { ClientId = fun () -> getRandomString 33
+          ClientSecret = fun () -> getRandomString 50
+          // TODO : Read ???
+          AuthorizationCode = fun () -> getRandomString 35 }
+
+    type AuthStringsProvider() =
+        interface IAuthStringsProvider with
+            member __.AuthStringsProvider = authStringsProvider
