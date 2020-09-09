8d58e4de0f9de4f6deb1457fb2929ab7b411bb39
baio <max.putilov@gmail.com>
2020-09-09 18:34:55 +0300

    cluster api + admin

---
+++ IdentityServer/Apps/PRR/PRR.API/Routes/PingRoutes.fs
@@ -0,0 +1,11 @@
+﻿namespace PRR.API.Routes
+
+module VersionRoutes =
+
+    let private recreatedDb ctx =
+        let dataContext = getDataContext ctx
+        dataContext.Database.EnsureDeleted() |> ignore
+        dataContext.Database.Migrate() |> ignore
+
+    let createRoutes () =
+        choose [ route "/e2e/reset"
