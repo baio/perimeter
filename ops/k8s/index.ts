367aa0ce1fb8c6802dde22ad7c23e5c5c2986b8c
baio <max.putilov@gmail.com>
2020-12-07 20:08:22 +0300

    create-api-image

--- ops/k8s/index.ts
+++ ops/k8s/index.ts
@@ -1,30 +1,8 @@
-import * as docker from '@pulumi/docker';
-import * as pulumi from '@pulumi/pulumi';
-import * as github from '@pulumi/github';
+import { config } from './config';
+import { createApiImage } from './create-api-image';
 
 // https://www.pulumi.com/docs/reference/pkg/docker/image/
 
-// The folder containing a Dockerfile.
-const customImage = '../../IdentityServer';
 const version = 'v0.21';
-const imageName = 'baio/prr-api';
 
-const config = new pulumi.Config();
-const registryServer = config.require('registryServer');
-const registryUsername = config.require('registryUsername');
-const registryPassword = config.require('registryPassword');
-
-const registry: docker.ImageRegistry = {
-    server: registryServer,
-    username: registryUsername,
-    password: registryPassword,
-};
-
-const myImage = new docker.Image(customImage, {
-    imageName: `${imageName}:${version}`,
-    build: {
-        context: `./${customImage}`,
-    },
-    registry,
-});
-export const name = myImage.imageName;
+export const apiImageName = createApiImage(version, config.registry);
